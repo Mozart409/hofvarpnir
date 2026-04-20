ac73503b033b4ad234f838ee22986a67c450ea3e
Amadeus Mader <amadeus@mozart409.com>
2026-04-20 14:37:47 +0200

    fix: show target size

--- flake.nix
+++ flake.nix
@@ -275,12 +275,14 @@
           ++ pkgs.lib.optionals pkgs.stdenv.isLinux linuxDevPackages
           ++ pkgs.lib.optionals pkgs.stdenv.isDarwin darwinDevPackages;
         shellHook = ''
-          lefthook install
-          cog install-hook
-          yt-dlp --version
           export COMPOSE_BAKE=true
           export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
           export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+          lefthook install
+          cog install-hook
+          yt-dlp --version
+          du -sh ./target
+          echo ""
         '';
       };
     });
